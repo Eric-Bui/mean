20a0658c080fd056691ac63d577f54d2a6a28814
AnhBui <buitheanh1990@gmail.com>
2020-09-10 00:34:31 +0700

    edit __dirname

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 3000;
 // Add headers
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(_dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.use(bodyParser.json()); // for parsing application/json
